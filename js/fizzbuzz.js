0431ed550e0e5f1707d941acc22607b79809abc0
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2017-08-31 15:18:26 +0530

    added fizzbuzz code with complexity

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -3,3 +3,19 @@
 // For numbers divisible by 3, print "Fizz" instead of the number,
 // and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+
+// complexity: O (n)
+function fizbuzz(n) {
+  for(let i = 0; i < n; i++) {
+    if (i % 5 && i % 3) {
+      console.log('FizzBuzz');
+    } else if (i % 5) {
+      console.log('Buzz');
+    } else if (i % 3) {
+      console.log('Fizz');
+    }
+  }
+}
+
+fizbuzz(100);
